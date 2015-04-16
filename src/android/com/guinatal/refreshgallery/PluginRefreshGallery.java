c9157e223c9c42063bb24f306c0a3f6852541c16
Guilherme Mello <guilhermemello@Guilhermes-MacBook-Pro.local>
2015-04-16 13:04:49 +0100

    1.0.6

--- src/android/com/guinatal/refreshgallery/PluginRefreshGallery.java
+++ src/android/com/guinatal/refreshgallery/PluginRefreshGallery.java
@@ -16,7 +16,7 @@
        specific language governing permissions and limitations
        under the License.
 */
-package nl.xservices.plugins;
+package com.guinatal.refreshgallery;
 
 import java.io.File;
 
